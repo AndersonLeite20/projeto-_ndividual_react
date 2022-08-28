f84529d3c9b59fffc638ba5ee34c8913926010ee
Anderson Leite <alsanderson3@gmail.com>
2022-08-28 19:52:40 -0300

    primeiro commit
    Já tinha criado alguns componentes, porém tive que criar um novo repositório remoto.

---
+++ my-app/src/App.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Img1 from "./img/pes01.jpg"
+import Img2 from "./img/pes02.jpg"
+import Img3 from "./img/pes03.jpg"
+import "./css/style.css"
+//import {View, Button, text} from 'react-native'
+
+
+function App(){
+  return (
+    <>
+    <div className="container">
+        <div className="div-block">
+            <div className="div-img">
+                <img id="img" src={Img1} alt="p"/>
+            </div>
+            <div className="div-nome">
+                <p>Vivian Santos</p>
+            </div>
+        </div>
+        <div className="div-bot">
+            <button>Pager</button>
+        </div>
+    </div>
+
+    <div className="container">
+        <div className="div-block">
+            <div className="div-img">
+                <img id="img" src={Img2} alt="p"/>
+            </div>
+            <div className="div-nome">
+                <p>Maria Paula</p>
+            </div>
+        </div>
+        <div className="div-bot">
+            <button>Pager</button>
+        </div>
+    </div>
+
+    <div className="container">
+        <div className="div-block">
+            <div className="div-img">
+                <img id="img" src={Img3} alt="p"/>
+            </div>
+            <div className="div-nome">
+                <p>Brumo Jorge</p>
+            </div>
+        </div>
+        <div className="div-bot">
+            <button type="submit">Pager</button>
+        </div>
+    </div>
+    </>
+  );
+}
+export default App;
---
+++ my-app/src/modal/modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../css/style.css"
+
+function modal(){
+    return(
+        <>
+        <div className="modal-conteiner">
+        <div className="div-nome-usuario">
+            <h3>Pagamento para NOME DO USUARIO</h3>
+        </div>
+        <div className="div-valor">
+            <input id="input-valor" type="text" placeholder="R$ 0,00"/>
+        </div>
+        <div className="div-select">
+            <select name="CARTAO" id="select" aria-placeholder="cartão com final"><option value="">CARTÃO COM O FINAL 0123</option></select>
+        </div>
+        <div className="div-button-pagar">
+            <button id="button">PAGAR</button>
+        </div>
+    </div>
+        </>
+    )
+}
+export default modal
